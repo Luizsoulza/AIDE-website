419560f5273080f705c2744d8272802912ab9f74
Luizsoulza <luizsouza1411@outlook.com>
2025-05-23 01:52:21 -0300

    Organizando Pastas

--- home/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ver_home/', views.ver_home, name='ver_home'),
-]
--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def ver_home(request):
-    return render(request, 'home/ver_home.html')  # Ajuste o caminho conforme a estrutura do seu template
